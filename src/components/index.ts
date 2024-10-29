0dc579acdc89e791b5bfdb24cb75c8c3b96dd3fc
Felipe DR <felipe.corp7@gmail.com>
2024-10-28 22:46:06 -0300

    feat(components): add imports

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export * from './footer/footer.component';
+export * from './header/header.component';
+export * from './logo/logo.component';
+export * from './mobile-menu-button/mobile-menu-button.component';
+export * from './navigation-menu-link/navigation-menu-link.component';
+export * from './navigation-menu/navigation-menu.component';
+export * from './ui/button';
