0f1da54b9f1ce6ad6aceac2a424ecaa46261d204
Felipe DR <felipe.corp7@gmail.com>
2024-10-31 21:14:28 -0300

    feat(app): add hero, provider and category component

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,27 @@
+import CategoryProvider from '@/contexts/category/category.context';
+
+import {
+  CategoryMenuComponent,
+  HeroComponent,
+  SectionBoxComponent,
+  TitleComponent,
+} from '@/components';
+
 export default function BlogPage(): JSX.Element {
-  return <h1>Dinamiques</h1>;
+  return (
+    <>
+      <HeroComponent>
+        <TitleComponent className="text-base-white md:ms-8 lg:ms-11" tag="h1">
+          Conhecimento ao alcance de todos
+        </TitleComponent>
+      </HeroComponent>
+      <main>
+        <CategoryProvider>
+          <SectionBoxComponent tag="section">
+            <CategoryMenuComponent />
+          </SectionBoxComponent>
+        </CategoryProvider>
+      </main>
+    </>
+  );
 }
