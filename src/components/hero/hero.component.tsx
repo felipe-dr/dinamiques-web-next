1eacea0fa8f8ff4f536291c261f28240007e37be
Felipe DR <felipe.corp7@gmail.com>
2024-10-31 21:25:28 -0300

    feat(components): implements HeroComponent

---
+++ src/components/hero/hero.component.tsx
@@ -0,0 +1,48 @@
+import Image from 'next/image';
+import { ComponentProps, JSX } from 'react';
+
+import { cn } from '@/libs/utils';
+
+import { SectionDecoratorComponent } from '../section-decorator/section-decorator.component';
+
+interface HeroComponentProps extends ComponentProps<'section'> {
+  backgroundImage?: {
+    path: string;
+    alt: string;
+    width: number;
+    height: number;
+    isPriority?: boolean;
+  };
+  children: React.ReactNode;
+}
+
+export function HeroComponent({
+  backgroundImage,
+  children,
+  ...props
+}: HeroComponentProps): JSX.Element {
+  return (
+    <section
+      {...props}
+      className={cn(
+        'relative bg-base-15 py-15 lg:py-[13.75rem]',
+        props.className,
+      )}
+    >
+      {backgroundImage && (
+        <Image
+          className="absolute top-0 -z-0 size-full object-cover"
+          src={backgroundImage.path}
+          alt={backgroundImage.alt}
+          width={backgroundImage.width}
+          height={backgroundImage.height}
+          priority={backgroundImage.isPriority}
+        />
+      )}
+      <div className="container relative z-[2] flex max-w-screen-xl items-center justify-between">
+        {children}
+      </div>
+      <SectionDecoratorComponent className="absolute bottom-0 z-[2] fill-base-16" />
+    </section>
+  );
+}
