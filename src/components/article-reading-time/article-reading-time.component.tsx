33415d577162aa94e9b1a529c0af244960ad0ccf
Felipe DR <felipe.corp7@gmail.com>
2024-11-03 10:52:36 -0300

    feat(components): implements ArticleReadingTimeComponent

---
+++ src/components/article-reading-time/article-reading-time.component.tsx
@@ -0,0 +1,22 @@
+import { ClockIcon } from '@heroicons/react/24/outline';
+import { ComponentProps } from 'react';
+
+import { formatToHoursAndMinutes } from '@/utils';
+
+interface ArticleReadingTimeComponentProps extends ComponentProps<'time'> {
+  readingTime: number;
+}
+
+export function ArticleReadingTimeComponent({
+  readingTime,
+}: ArticleReadingTimeComponentProps): JSX.Element {
+  return (
+    <time
+      className="flex items-center gap-2 text-sm font-semibold text-base-4"
+      dateTime={`${readingTime}`}
+    >
+      <ClockIcon className="size-[1.042rem] text-primary-3" />
+      {formatToHoursAndMinutes(readingTime)}
+    </time>
+  );
+}
