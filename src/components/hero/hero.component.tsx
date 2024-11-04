1e0d5156120d79cdf5b0d468c61137837c8862bb
Felipe DR <felipe.corp7@gmail.com>
2024-11-04 18:50:11 -0300

    feat(components): add parts of the HeroComponent

--- src/components/hero/hero.component.tsx
+++ src/components/hero/hero.component.tsx
@@ -39,10 +39,71 @@ export function HeroComponent({
           priority={backgroundImage.isPriority}
         />
       )}
-      <header className="container relative z-[2] flex max-w-screen-xl items-center justify-between">
-        {children}
-      </header>
+      {children}
       <SectionDecoratorComponent className="absolute bottom-0 z-[2] fill-base-16" />
     </section>
+  );
+}
+
+interface HeroHeaderComponentProps extends ComponentProps<'header'> {
+  children: React.ReactNode;
+}
+
+export function HeroHeaderComponent({
+  children,
+  ...props
+}: HeroHeaderComponentProps): JSX.Element {
+  return (
+    <header
+      {...props}
+      className={cn(
+        'container relative z-[1] flex max-w-screen-xl items-center justify-between',
+        props.className,
+      )}
+    >
+      {children}
+    </header>
+  );
+}
+
+interface HeroContentComponentProps extends ComponentProps<'div'> {
+  children: React.ReactNode;
+}
+
+export function HeroContentComponent({
+  children,
+  ...props
+}: HeroContentComponentProps): JSX.Element {
+  return (
+    <div
+      {...props}
+      className={cn(
+        'container relative z-[1] flex max-w-screen-xl items-center',
+        props.className,
+      )}
+    >
+      {children}
+    </div>
+  );
+}
+
+interface HeroFooterComponentProps extends ComponentProps<'footer'> {
+  children: React.ReactNode;
+}
+
+export function HeroFooterComponent({
+  children,
+  ...props
+}: HeroFooterComponentProps): JSX.Element {
+  return (
+    <footer
+      {...props}
+      className={cn(
+        'container relative z-[1] flex max-w-screen-xl items-center',
+        props.className,
+      )}
+    >
+      {children}
+    </footer>
   );
 }
