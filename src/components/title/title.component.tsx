5268bf66fa609b913b9f7942acc9197f0bed4123
Felipe DR <felipe.corp7@gmail.com>
2024-11-05 19:50:36 -0300

    style(components): add h4, h5 and h6

--- src/components/title/title.component.tsx
+++ src/components/title/title.component.tsx
@@ -9,6 +9,9 @@ const titleVariant = cva('w-full break-words font-semibold uppercase', {
       h1: 'xs:text-h1-xs sm:max-w-[35rem] md:max-w-[51.25rem] md:text-h1-md lg:text-h1-lg',
       h2: 'xs:text-h2-xs md:text-h2-md lg:text-h2-lg',
       h3: 'xs:text-h3-xs md:text-h3-md lg:text-h3-lg',
+      h4: 'xs:text-h3-xs md:text-h3-md lg:text-h3-lg',
+      h5: 'xs:text-h3-xs md:text-h3-md lg:text-h3-lg',
+      h6: 'xs:text-h3-xs md:text-h3-md lg:text-h3-lg',
     },
     decorator: {
       true: 'relative ps-4 before:absolute before:left-0 before:h-full before:w-[0.25rem] before:bg-primary-7',
@@ -20,7 +23,7 @@ const titleVariant = cva('w-full break-words font-semibold uppercase', {
 });
 
 interface TitleComponentProps extends ComponentProps<'h1'> {
-  tag: 'h1' | 'h2' | 'h3';
+  tag: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
   hasBarDecorator?: boolean;
   hasDotDecorator?: boolean;
   children: React.ReactNode;
