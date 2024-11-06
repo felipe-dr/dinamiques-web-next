28f3417712b5b75189b3b5bb6bce2f1f31bf612a
Felipe DR <felipe.corp7@gmail.com>
2024-11-06 19:31:26 -0300

    feat(components): add onChange to filter and change imports

--- src/components/search-input/search-input.component.tsx
+++ src/components/search-input/search-input.component.tsx
@@ -1,13 +1,19 @@
+'use client';
+
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { ComponentProps, JSX } from 'react';
 
-import { cn } from '@/libs/tailwind/tailwind.lib';
+import { useArticleContext } from '@/contexts';
 
-import { InputComponent } from '../ui/input';
+import { cn } from '@/libs';
+
+import { InputComponent } from '@/components';
 
 export function SearchInputComponent({
   ...props
 }: ComponentProps<'input'>): JSX.Element {
+  const { searchQuery, handleArticleFilter } = useArticleContext();
+
   return (
     <>
       <InputComponent
@@ -15,6 +21,10 @@ export function SearchInputComponent({
         className={cn(props.className)}
         type="search"
         placeholder="Buscar por título ou conteúdo"
+        value={searchQuery}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+          handleArticleFilter(event.target.value)
+        }
       />
       <MagnifyingGlassIcon className="absolute right-4 top-1/2 size-[1.042rem] -translate-y-1/2 text-primary-3 lg:right-5 lg:size-[1.34rem]" />
     </>
