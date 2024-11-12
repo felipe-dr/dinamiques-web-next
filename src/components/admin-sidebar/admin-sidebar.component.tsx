1d58ecd36f5b71c05ed13c8fd8fdb2d37ae4a4d3
Felipe DR <felipe.corp7@gmail.com>
2024-11-12 20:54:21 -0300

    feat(components): implements AdminSidebarComponent

---
+++ src/components/admin-sidebar/admin-sidebar.component.tsx
@@ -0,0 +1,68 @@
+import {
+  ArrowLeftEndOnRectangleIcon,
+  DocumentTextIcon,
+  TagIcon,
+} from '@heroicons/react/24/outline';
+import Link from 'next/link';
+
+import {
+  SidebarComponent,
+  SidebarContentComponent,
+  SidebarFooterComponent,
+  SidebarGroupComponent,
+  SidebarGroupContentComponent,
+  SidebarGroupLabelComponent,
+  SidebarHeaderComponent,
+  SidebarMenuButtonComponent,
+  SidebarMenuComponent,
+  SidebarMenuItemComponent,
+} from '@/components';
+
+const ADMIN_SIDEBAR_ITEMS = [
+  {
+    label: 'categorias',
+    path: '/admin/categories',
+    icon: TagIcon,
+  },
+  {
+    label: 'artigos',
+    path: '/admin/articles',
+    icon: DocumentTextIcon,
+  },
+];
+
+export function AdminSidebarComponent(): JSX.Element {
+  return (
+    <SidebarComponent>
+      <SidebarHeaderComponent />
+      <SidebarContentComponent>
+        <SidebarGroupComponent>
+          <SidebarGroupLabelComponent>Gestão</SidebarGroupLabelComponent>
+          <SidebarGroupContentComponent>
+            <SidebarMenuComponent>
+              {ADMIN_SIDEBAR_ITEMS.map((item) => (
+                <SidebarMenuItemComponent
+                  className="capitalize"
+                  key={item.label}
+                >
+                  <SidebarMenuButtonComponent asChild>
+                    <Link href={item.path}>
+                      <item.icon />
+                      {item.label}
+                    </Link>
+                  </SidebarMenuButtonComponent>
+                </SidebarMenuItemComponent>
+              ))}
+            </SidebarMenuComponent>
+          </SidebarGroupContentComponent>
+        </SidebarGroupComponent>
+      </SidebarContentComponent>
+      <SidebarFooterComponent className="flex-row justify-between px-4 text-sm">
+        <span>João Silva</span>
+        <button type="button">
+          <ArrowLeftEndOnRectangleIcon className="size-5" />
+        </button>
+      </SidebarFooterComponent>
+    </SidebarComponent>
+  );
+}
