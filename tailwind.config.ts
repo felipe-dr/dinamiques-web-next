5b953920dc4bd6784af9c23b9a88f0dd2ca89534
Felipe DR <felipe.corp7@gmail.com>
2024-11-12 20:46:21 -0300

    build(tailwind): add sidebar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -67,6 +67,16 @@ const config: Config = {
         6: 'rgba(var(--error-6), <alpha-value>)',
         7: 'rgba(var(--error-7), <alpha-value>)',
       },
+      sidebar: {
+        DEFAULT: 'hsl(var(--sidebar-background))',
+        foreground: 'hsl(var(--sidebar-foreground))',
+        primary: 'hsl(var(--sidebar-primary))',
+        'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+        accent: 'hsl(var(--sidebar-accent))',
+        'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+        border: 'hsl(var(--sidebar-border))',
+        ring: 'hsl(var(--sidebar-ring))',
+      },
     },
     fontFamily: {
       inter: 'var(--font-inter)',
