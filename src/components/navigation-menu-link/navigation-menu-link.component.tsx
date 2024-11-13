1208e89bc0f2cd1daf361f07f90a00c023a1f8c1
Felipe DR <felipe.corp7@gmail.com>
2024-11-12 21:12:53 -0300

    style(components): add hover

--- src/components/navigation-menu-link/navigation-menu-link.component.tsx
+++ src/components/navigation-menu-link/navigation-menu-link.component.tsx
@@ -27,7 +27,7 @@ export function NavigationMenuLinkComponent({
     <li>
       <Link
         className={cn(
-          `${buttonVariants({ variant: 'ghost' })} ${isActiveLink ? 'bg-base-14 font-semibold text-base-white md:border md:border-base-13 md:text-base-white' : 'text-base-6'} block px-5 py-4 capitalize md:rounded-lg md:bg-transparent md:px-4 md:py-3 md:text-sm lg:px-6 lg:text-lg `,
+          `${buttonVariants({ variant: 'ghost' })} ${isActiveLink ? 'bg-base-14 font-semibold text-base-white md:border md:border-base-13 md:text-base-white' : 'text-base-6'} hover:bg-transparent hover:text-base-white block px-5 py-4 capitalize md:rounded-lg md:bg-transparent md:px-4 md:py-3 md:text-sm lg:px-6 lg:text-lg `,
         )}
         href={path}
         onClick={onClick}
