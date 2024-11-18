dd0c0368f492c7f95505a71fd2f58362ce653267
Felipe DR <felipe.corp7@gmail.com>
2024-11-18 15:23:56 -0300

    feat(models): add import

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,5 @@
 export * from './api-response/api-response.model';
 export * from './article/article.model';
+export * from './auth/auth.model';
 export * from './category/category.model';
 export * from './teacher/teacher.model';
