f11c39492421087ccfce330c2e506dbf1b010422
Felipe DR <felipe.corp7@gmail.com>
2024-11-18 15:36:56 -0300

    feat(app): implements signin route and cookie logic

---
+++ src/app/api/auth/signin/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from 'next/server';
+
+import { ApiResponseModel, AuthModel } from '@/models';
+
+export async function POST(request: Request) {
+  const API_URL = process.env.NEXT_PUBLIC_DINAMIQUES_API;
+  const { email, password } = await request.json();
+
+  try {
+    const response = await fetch(`${API_URL}/users/signin`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (!response.ok) {
+      return response;
+    }
+
+    const { data }: ApiResponseModel<AuthModel> = await response.json();
+    const nextResponse = NextResponse.json({
+      ...data,
+    });
+
+    nextResponse.cookies.set('accessToken', data!.accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      path: '/',
+    });
+
+    return nextResponse;
+  } catch (error: unknown) {
+    return NextResponse.json({ error });
+  }
+}
