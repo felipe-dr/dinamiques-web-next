045e2e9218b944b6a11dfd6a36ce4e744aa5ae7b
Felipe DR <felipe.corp7@gmail.com>
2024-11-19 17:09:42 -0300

    feat(models): implements UserModel

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,3 +3,4 @@ export * from './article/article.model';
 export * from './auth/auth.model';
 export * from './category/category.model';
 export * from './teacher/teacher.model';
+export * from './user/user.model';
