e6ff9ec1b9b346d6762de4a4b010c256abd967ac
Felipe DR <felipe.corp7@gmail.com>
2024-11-19 16:37:53 -0300

    feat(components): implementButtonSigninOrEnterAdminComponent

---
+++ src/components/button-signin-or-enter-admin/button-signin-or-enter-admin.component.tsx
@@ -0,0 +1,24 @@
+'use server';
+
+import { cookies } from 'next/headers';
+import Link from 'next/link';
+
+import { cn } from '@/libs';
+
+import { SigninDialogComponent } from '../signin-dialog/signin-dialog.component';
+import { buttonVariants } from '../ui/button';
+
+export async function ButtonSigninOrEnterAdminComponent(): Promise<JSX.Element> {
+  const accesToken = cookies().get('accessToken')?.value;
+
+  return accesToken ? (
+    <Link
+      className={cn(buttonVariants({ color: 'primary' }), 'w-full')}
+      href="/admin"
+    >
+      Admin
+    </Link>
+  ) : (
+    <SigninDialogComponent />
+  );
+}
