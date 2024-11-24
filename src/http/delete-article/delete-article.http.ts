9bcfdca7aaf8d9354cb4ba9fb662677e8e35a10b
Felipe DR <felipe.corp7@gmail.com>
2024-11-24 14:15:58 -0300

    feat(http): implements deleteArticleHttp

---
+++ src/http/delete-article/delete-article.http.ts
@@ -0,0 +1,36 @@
+import { ApiResponseModel } from '@/models';
+
+import { API_URL } from '../api-client/api-client.http';
+
+interface DeleteArticleHttpRequest {
+  id: string;
+  accessToken: string;
+}
+
+type DeleteArticleHttpResponse = ApiResponseModel | undefined;
+
+export async function deleteArticleHttp({
+  id,
+  accessToken,
+}: DeleteArticleHttpRequest): Promise<DeleteArticleHttpResponse> {
+  const response = await fetch(`${API_URL}/articles/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  const { statusCode, error, message }: ApiResponseModel =
+    await response.json();
+
+  if (!response.ok) {
+    throw {
+      statusCode,
+      error,
+      message,
+    };
+  }
+
+  return { statusCode, error, message };
+}
