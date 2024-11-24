cc16bd97b2c39b0fdf999a3feb7c8ed6b61f6be6
Felipe DR <felipe.corp7@gmail.com>
2024-11-24 14:15:42 -0300

    feat(http): implements createArticleHttp

---
+++ src/http/create-article/create-article.http.ts
@@ -0,0 +1,47 @@
+import { ApiResponseModel } from '@/models';
+
+import { API_URL } from '../api-client/api-client.http';
+
+interface Article {
+  categoryId: string;
+  title: string;
+  summary: string;
+  readingTime: number;
+  content: string;
+  highlightImageUrl?: string | undefined;
+}
+
+interface CreateArticleHttpRequest {
+  accessToken: string;
+  article: Article;
+}
+
+type CreateArticleHttpResponse = ApiResponseModel | undefined;
+
+export async function createArticleHttp({
+  accessToken,
+  article,
+}: CreateArticleHttpRequest): Promise<CreateArticleHttpResponse> {
+  const url = `${API_URL}/articles`;
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+    body: JSON.stringify(article),
+  });
+
+  const { statusCode, error, message }: ApiResponseModel =
+    await response.json();
+
+  if (!response.ok) {
+    throw {
+      statusCode,
+      error,
+      message,
+    };
+  }
+
+  return { statusCode, error, message };
+}
