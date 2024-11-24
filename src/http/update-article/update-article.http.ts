4a788099cb8e9a1d59484c2e465e85fea35e9328
Felipe DR <felipe.corp7@gmail.com>
2024-11-24 14:18:19 -0300

    feat(http): implements updateArticleHttp

---
+++ src/http/update-article/update-article.http.ts
@@ -0,0 +1,52 @@
+import { ApiResponseModel } from '@/models';
+
+import { API_URL } from '../api-client/api-client.http';
+
+interface Article {
+  id?: string;
+  categoryId: string;
+  title: string;
+  summary: string;
+  readingTime: number;
+  content: string;
+  highlightImageUrl?: string;
+  isPublished?: boolean;
+}
+
+interface UpdateArticleHttpRequest {
+  accessToken: string;
+  article: Article;
+}
+
+type UpdateArticleHttpResponse = ApiResponseModel | undefined;
+
+export async function updateArticleHttp({
+  accessToken,
+  article,
+}: UpdateArticleHttpRequest): Promise<UpdateArticleHttpResponse> {
+  const url = `${API_URL}/articles/${article.id}`;
+
+  delete article.id;
+
+  const response = await fetch(url, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+    body: JSON.stringify(article),
+  });
+
+  const { statusCode, error, message }: ApiResponseModel =
+    await response.json();
+
+  if (!response.ok) {
+    throw {
+      statusCode,
+      error,
+      message,
+    };
+  }
+
+  return { statusCode, error, message };
+}
