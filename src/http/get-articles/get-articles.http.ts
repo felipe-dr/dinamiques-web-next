8610ddf39d37ba4714ed288916de540d959bf4a1
Felipe DR <felipe.corp7@gmail.com>
2024-11-24 14:16:38 -0300

    feat(http): implements getArticlesHttp

---
+++ src/http/get-articles/get-articles.http.ts
@@ -0,0 +1,19 @@
+import { ApiResponseWithDataModel, ArticleModel } from '@/models';
+
+import { API_URL } from '../api-client/api-client.http';
+
+type GetArticlesHttpResponse = ArticleModel[] | undefined;
+
+export async function getArticlesHttp(): Promise<GetArticlesHttpResponse> {
+  const response = await fetch(`${API_URL}/articles`);
+
+  // TODO: change to friendly error
+  if (!response.ok) {
+    throw new Error('Failed to fetch articles');
+  }
+
+  const { data }: ApiResponseWithDataModel<ArticleModel[]> =
+    await response.json();
+
+  return data;
+}
