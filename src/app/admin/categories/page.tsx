825a3f8f3a6174bb33e78bb4c2cf122360bc3395
Felipe DR <felipe.corp7@gmail.com>
2024-11-25 20:27:26 -0300

    refactor(app): change ButtonComponent to Link

--- src/app/admin/categories/page.tsx
+++ src/app/admin/categories/page.tsx
@@ -1,21 +1,16 @@
-'use client';
-
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 import { cn } from '@/libs';
 
 import {
   AdminContentWrapperHeaderComponent,
   AdminContentWrapperSectionComponent,
-  ButtonComponent,
   buttonVariants,
   CategoriesTableComponent,
   NavigationBreadcrumbComponent,
 } from '@/components';
 
 export default function CategoriesAdminPage(): JSX.Element {
-  const router = useRouter();
-
   const breadcrumbItems = [
     {
       label: 'categorias',
@@ -30,15 +25,15 @@ export default function CategoriesAdminPage(): JSX.Element {
           className="m-0 mb-3 md:m-0 lg:m-0"
           breadcrumbItems={breadcrumbItems}
         />
-        <ButtonComponent
+        <Link
           className={cn(
             buttonVariants({ variant: 'admin', color: 'primary' }),
             'justify-self-end',
           )}
-          onClick={() => router.push('/admin/categories/add')}
+          href="/admin/categories/add"
         >
           Adicionar
-        </ButtonComponent>
+        </Link>
       </AdminContentWrapperHeaderComponent>
       <AdminContentWrapperSectionComponent>
         <CategoriesTableComponent />
