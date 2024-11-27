3f0750e354b4a7af148bb1ec006b00c8fd0ea527
Felipe DR <felipe.corp7@gmail.com>
2024-11-27 18:55:32 -0300

    perf(components): add microdata

--- src/components/blog/article-card/article-card.component.tsx
+++ src/components/blog/article-card/article-card.component.tsx
@@ -36,6 +36,8 @@ export function ArticleCardComponent({
       <CardComponent
         className="grid overflow-hidden duration-500 ease-in-out group-hover:border-base-11"
         as="article"
+        itemScope
+        itemType="https://schema.org/Article"
       >
         <div
           className={`relative flex h-[13.75rem] overflow-hidden ${!article.highlightImageUrl && 'bg-primary-4'}`}
@@ -47,6 +49,7 @@ export function ArticleCardComponent({
               width={440}
               height={220}
               alt={article.title}
+              itemProp="image"
             />
           )}
           <SectionDecoratorComponent className="absolute bottom-0 z-[2] h-[2.5rem] fill-base-15" />
@@ -55,6 +58,7 @@ export function ArticleCardComponent({
           <h2
             className="mb-3 border-s-2 ps-1 text-xs font-semibold uppercase tracking-[0.038em] text-base-white"
             style={{ borderColor: category.color }}
+            itemProp="genre"
           >
             {category.name}
           </h2>
@@ -63,12 +67,15 @@ export function ArticleCardComponent({
             tag="h1"
             hasBarDecorator={false}
             hasDotDecorator={false}
+            itemProp="headline"
           >
             {article.title}
           </TitleComponent>
         </CardHeaderComponent>
         <CardContentComponent>
-          <p className="text-md">{article.summary}</p>
+          <p className="text-md" itemProp="description">
+            {article.summary}
+          </p>
         </CardContentComponent>
         <CardFooterComponent className="flex flex-wrap items-center justify-between gap-5 self-end pt-11">
           <div className="flex items-center gap-x-3">
@@ -83,7 +90,13 @@ export function ArticleCardComponent({
               )}
             </AvatarComponent>
             <div className="grid gap-y-1">
-              <ArticleAuthorNameComponent tag="h3" authorName={teacher.name} />
+              <ArticleAuthorNameComponent
+                tag="h3"
+                authorName={teacher.name}
+                itemProp="author"
+                itemScope
+                itemType="https://schema.org/Person"
+              />
               <ArticlePublishedLastDateComponent
                 publishedLastDate={article.publishedLastDate}
               />
