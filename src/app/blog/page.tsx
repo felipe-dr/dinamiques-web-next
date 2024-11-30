1ddb23656c8d9bcb5dd0bcfc12d5099a67be91bd
Felipe DR <felipe.corp7@gmail.com>
2024-11-30 10:27:34 -0300

    feat(app): add animation and change get data

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -20,17 +20,20 @@ export default async function BlogPage(): Promise<JSX.Element> {
     <>
       <HeroComponent>
         <HeroHeaderComponent>
-          <TitleComponent className="text-base-white md:ms-8 lg:ms-11" tag="h1">
+          <TitleComponent
+            className="animate-fadein text-base-white opacity-0 md:ms-8 lg:ms-11"
+            tag="h1"
+          >
             Conhecimento ao alcance de todos
           </TitleComponent>
         </HeroHeaderComponent>
       </HeroComponent>
       <main>
         <CategoryProvider>
-          <ArticleProvider fetchedArticles={articles!}>
+          <ArticleProvider fetchedArticles={articles!.data!}>
             <SectionBoxComponent className="pb-0 lg:pb-0" tag="section">
               {categories ? (
-                <CategoryMenuComponent categories={categories} />
+                <CategoryMenuComponent categories={categories!.data!} />
               ) : (
                 <p>Não há categorias disponíveis no momento.</p>
               )}
