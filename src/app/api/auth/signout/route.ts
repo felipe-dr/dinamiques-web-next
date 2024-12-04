296654fb00adf9a43c55570ed0ca14c25f331efc
Felipe DR <felipe.corp7@gmail.com>
2024-12-04 20:43:26 -0300

    refactor(app): change to remove cookie

--- src/app/api/auth/signout/route.ts
+++ src/app/api/auth/signout/route.ts
@@ -1,15 +1,11 @@
-import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 
 export async function POST() {
-  cookies().set('accessToken', '', {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
-    path: '/',
+  const response = NextResponse.json({
+    isAuthenticated: false,
   });
 
-  return new NextResponse(
-    JSON.stringify({ message: 'Logout realizado com sucesso.' }),
-  );
+  response.cookies.delete('accessToken');
+
+  return response;
 }
